2844a15a7ce556cb4ca73412e66434dada0bc164
Travis Gabriel <tgabriel@usgs.gov>
2021-09-22 17:37:04 -0700

    Help diagnose remote test failing

--- libpyhat/transform/tests/test_transforms.py
+++ libpyhat/transform/tests/test_transforms.py
@@ -402,6 +402,9 @@ def test_dimred_LFDA_usingLIBS():
     #by comparing their distance to 10000 times their combined standard deviations
     np.testing.assert_array_less(s1_bas_and*1000, np.abs(m1_bas-m1_and))
     
+    #Troubleshooting print statement. Local testing passes, remote doesn't. Something's up...
+    print(df, df['LFDA (wvl)']['LFDA-1'].values[ind_bas], df['LFDA (wvl)']['LFDA-1'].values[ind_and])
+    
     #Let's also include a direct value test of the means
     np.testing.assert_array_almost_equal([m1_bas.real, m1_and.real], [207176884.18443012, -1434710064.2183135])
 
