2c97719f1556204df571243246a7c7de453d3cfd
Travis Gabriel <tgabriel@usgs.gov>
2021-09-28 21:06:53 -0700

    increasing precision of the test to pass array almost equal test

--- libpyhat/transform/tests/test_transforms.py
+++ libpyhat/transform/tests/test_transforms.py
@@ -126,8 +126,8 @@ def test_dimred_tSNE():
         'init': 'pca'}
     df, dimred_obj  = dim_red.dim_red(df, 'wvl', 't-SNE', [], params)
     expected_div    = 0.41363900899887085
-    expected_scores = [9398.069, -73.208405]
-
+    expected_scores = [9398.069336, -73.208405]
+    
     assert df['t-SNE (wvl)'].shape == (103, 2)
     np.testing.assert_almost_equal(expected_div, dimred_obj.kl_divergence_, decimal=6)
     np.testing.assert_array_almost_equal(expected_scores, np.array(df['t-SNE (wvl)'].iloc[0, :]))
